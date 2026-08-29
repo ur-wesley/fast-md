0cb452bb87a2e19d88866bc32cbf6557b257198f
ur-wesley <ur-wesley@users.noreply.github.com>
2026-08-29 17:26:29 +0200

    chore(deps): bump Dioxus 0.7 and add primitives

---
+++ src/ui/context_menu/component.rs
@@ -0,0 +1,59 @@
+use dioxus::prelude::*;
+use dioxus_primitives::context_menu::{
+    self, ContextMenuContentProps, ContextMenuItemProps, ContextMenuProps, ContextMenuTriggerProps,
+};
+
+#[css_module("/src/ui/context_menu/style.css")]
+struct Styles;
+
+#[component]
+pub fn ContextMenu(props: ContextMenuProps) -> Element {
+    rsx! {
+        context_menu::ContextMenu {
+            disabled: props.disabled,
+            open: props.open,
+            default_open: props.default_open,
+            on_open_change: props.on_open_change,
+            roving_loop: props.roving_loop,
+            attributes: props.attributes,
+            {props.children}
+        }
+    }
+}
+
+#[component]
+pub fn ContextMenuTrigger(props: ContextMenuTriggerProps) -> Element {
+    rsx! {
+        context_menu::ContextMenuTrigger {
+            attributes: props.attributes,
+            {props.children}
+        }
+    }
+}
+
+#[component]
+pub fn ContextMenuContent(props: ContextMenuContentProps) -> Element {
+    rsx! {
+        context_menu::ContextMenuContent {
+            class: Styles::dx_context_menu_content,
+            id: props.id,
+            attributes: props.attributes,
+            {props.children}
+        }
+    }
+}
+
+#[component]
+pub fn ContextMenuItem(props: ContextMenuItemProps) -> Element {
+    rsx! {
+        context_menu::ContextMenuItem {
+            class: Styles::dx_context_menu_item,
+            disabled: props.disabled,
+            value: props.value,
+            index: props.index,
+            on_select: props.on_select,
+            attributes: props.attributes,
+            {props.children}
+        }
+    }
+}
---
+++ src/ui/toggle_group/component.rs
@@ -0,0 +1,36 @@
+use dioxus::prelude::*;
+use dioxus_primitives::toggle_group::{self, ToggleGroupProps, ToggleItemProps};
+
+#[css_module("/src/ui/toggle_group/style.css")]
+struct Styles;
+
+#[component]
+pub fn ToggleGroup(props: ToggleGroupProps) -> Element {
+    rsx! {
+        toggle_group::ToggleGroup {
+            class: Styles::dx_toggle_group,
+            default_pressed: props.default_pressed,
+            pressed: props.pressed,
+            on_pressed_change: props.on_pressed_change,
+            disabled: props.disabled,
+            allow_multiple_pressed: props.allow_multiple_pressed,
+            horizontal: props.horizontal,
+            roving_loop: props.roving_loop,
+            attributes: props.attributes,
+            {props.children}
+        }
+    }
+}
+
+#[component]
+pub fn ToggleItem(props: ToggleItemProps) -> Element {
+    rsx! {
+        toggle_group::ToggleItem {
+            class: Styles::dx_toggle_item,
+            index: props.index,
+            disabled: props.disabled,
+            attributes: props.attributes,
+            {props.children}
+        }
+    }
+}
